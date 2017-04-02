ec698b4d000c932597bbdcf11d8dbced86d91d47
Alexander <Alexander.Rogin@gmail.com>
2017-04-02 08:16:46 +0000

    Fixed paths. First version of model

--- clone.py
+++ clone.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('data/driving_log.csv') as csvfile:
+with open('./data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -12,8 +12,12 @@ images = []
 measurements = []
 for line in lines:
 	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = 'data/IMG/' + filename
+	if '\\' in source_path:
+		delimiter = '\\'
+	else:
+		delimiter = '/'
+	filename = source_path.split(delimiter)[-1]
+	current_path = './data/IMG/' + filename
 	image = cv2.imread(current_path)
 	images.append(image)
 	measurement = float(line[3])
@@ -30,6 +34,6 @@ model.add(Flatten(input_shape=(160, 320, 3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
 model.save('model.h5')
