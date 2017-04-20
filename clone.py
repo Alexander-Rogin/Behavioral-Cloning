e46b2e3e1367025444ed2ebb3b12f3dbb3a81950
Alexander <Alexander.Rogin@gmail.com>
2017-04-20 15:12:13 +0000

    data augmentation

--- clone.py
+++ clone.py
@@ -20,8 +20,11 @@ for line in lines:
 	current_path = './data/IMG/' + filename
 	image = cv2.imread(current_path)
 	images.append(image)
+	images.append(cv2.flip(image, 1))
+
 	measurement = float(line[3])
 	measurements.append(measurement)
+	measurements.append(-measurement)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -35,7 +38,6 @@ model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=shape))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
-#
 model.add(Convolution2D(6, (5, 5), input_shape=shape, activation="relu"))
 model.add(MaxPooling2D())
 
@@ -54,6 +56,6 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=15)
 
 model.save('model.h5')
