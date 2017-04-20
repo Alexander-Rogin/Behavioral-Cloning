828a624fb44fbf92322420d7fbf7fed5758b0058
Alexander <Alexander.Rogin@gmail.com>
2017-04-20 11:59:45 +0000

    basic lenet

--- clone.py
+++ clone.py
@@ -27,14 +27,33 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Cropping2D, Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Dropout
+
+shape = (160, 320, 3)
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=shape))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+#
+model.add(Convolution2D(6, (5, 5), input_shape=shape, activation="relu"))
+model.add(MaxPooling2D())
+
+model.add(Convolution2D(6, (5, 5), activation="relu"))
+model.add(MaxPooling2D())
+
+model.add(Convolution2D(6, (5, 5), activation="relu"))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(6, (5, 5), activation="relu"))
+model.add(MaxPooling2D())
+
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 
 model.save('model.h5')
