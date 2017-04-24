fc8b1fe019646c07d0e5b5e5d45537e2377c4f4d
Alexander Rogin <gonetc99@gmail.com>
2017-04-24 13:25:16 +0300

    Fixing indentations

--- clone.py
+++ clone.py
@@ -63,49 +63,49 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 def generator(samples, batch_size=32, useAugmented=False, useMultipleCameras=False):
 	num_samples = len(samples)
 	while 1: # Loop forever so the generator never terminates
-			shuffle(samples)
-			for offset in range(0, num_samples, batch_size):
-					batch_samples = samples[offset:offset+batch_size]
+		shuffle(samples)
+		for offset in range(0, num_samples, batch_size):
+			batch_samples = samples[offset:offset+batch_size]
 
 			images = []
 			angles = []
-				for batch_sample in batch_samples:
-					name = './IMG/'+batch_sample[0].split('/')[-1]
-					center_image = cv2.imread(name)
-					center_angle = float(batch_sample[3])
-					images.append(center_image)
-					angles.append(center_angle)
-
-			if useAugmented:
-				images.append(cv2.flip(center_image, 1))
-						angles.append(-center_angle)
-
-			 if useMultipleCameras:
-								correction = 0.2
-
-				name = './IMG/'+batch_sample[1].split('/')[-1]
-								left_image = cv2.imread(name)
-								left_angle = center_angle + correction
-
-				name = './IMG/'+batch_sample[2].split('/')[-1]
-								right_image = cv2.imread(name)
-								right_angle = center_angle - correction
-
-				images.append(left_image)
-							angles.append(left_angle)
-				images.append(right_image)
-								angles.append(right_angle)
+			for batch_sample in batch_samples:
+				name = './IMG/'+batch_sample[0].split('/')[-1]
+				center_image = cv2.imread(name)
+				center_angle = float(batch_sample[3])
+				images.append(center_image)
+				angles.append(center_angle)
 
 				if useAugmented:
-									images.append(cv2.flip(left_image, 1))
-									angles.append(-left_angle)
-					images.append(cv2.flip(right_image, 1))
-										angles.append(-right_angle)
+					images.append(cv2.flip(center_image, 1))
+					angles.append(-center_angle)
 
-				# trim image to only see section with road
-				X_train = np.array(images)
-				y_train = np.array(angles)
-				yield sklearn.utils.shuffle(X_train, y_train)
+			 	if useMultipleCameras:
+					correction = 0.2
+
+					name = './IMG/'+batch_sample[1].split('/')[-1]
+					left_image = cv2.imread(name)
+					left_angle = center_angle + correction
+
+					name = './IMG/'+batch_sample[2].split('/')[-1]
+					right_image = cv2.imread(name)
+					right_angle = center_angle - correction
+
+					images.append(left_image)
+					angles.append(left_angle)
+					images.append(right_image)
+					angles.append(right_angle)
+
+					if useAugmented:
+						images.append(cv2.flip(left_image, 1))
+						angles.append(-left_angle)
+						images.append(cv2.flip(right_image, 1))
+						angles.append(-right_angle)
+
+			# trim image to only see section with road
+			X_train = np.array(images)
+			y_train = np.array(angles)
+			yield sklearn.utils.shuffle(X_train, y_train)
 
 
 # compile and train the model using the generator function
@@ -124,9 +124,7 @@ ch, row, col = 3, 80, 320  # Trimmed image format
 
 model = Sequential()
 #model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=shape))
-model.add(Lambda(lambda x: x/127.5 - 1.,
-		input_shape=(ch, row, col),
-		output_shape=(ch, row, col)))
+model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(ch, row, col), output_shape=(ch, row, col)))
 #model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
 model.add(Convolution2D(6, (5, 5), input_shape=shape, activation="relu"))
@@ -148,8 +146,6 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 #model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=15)
-model.fit_generator(train_generator, samples_per_epoch= /
-			len(train_samples), validation_data=validation_generator, /
-			nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch= / len(train_samples), validation_data=validation_generator, / nb_val_samples=len(validation_samples), nb_epoch=1)
 
 #model.save('model.h5')
