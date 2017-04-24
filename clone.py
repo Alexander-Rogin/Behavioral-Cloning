998490b1fe915301e75c38ad5d78551c1c0cb36d
Alexander Rogin <gonetc99@gmail.com>
2017-04-24 13:27:30 +0300

    Cleaning

--- clone.py
+++ clone.py
@@ -146,6 +146,6 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 #model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=15)
-model.fit_generator(train_generator, samples_per_epoch= / len(train_samples), validation_data=validation_generator, / nb_val_samples=len(validation_samples), nb_epoch=1)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=1)
 
 #model.save('model.h5')
