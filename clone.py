c95a007a018f5daa983475925f0a7694486977f2
Alexander <Alexander.Rogin@gmail.com>
2017-04-28 09:18:53 +0000

    SAVEPOINT

--- clone.py
+++ clone.py
@@ -14,15 +14,15 @@ def read_image(imagePath):
 
 def process_image(images, measurements, imagePath, measurement, useAugmented):
 	image = read_image(imagePath)
-#	images.append(image)
-#	measurements.append(measurement)
+	images.append(image)
+	measurements.append(measurement)
 
-	if useAugmented and random.randint(0, 1) == 1:
+	if useAugmented: #and random.randint(0, 1) == 1:
 		images.append(cv2.flip(image, 1))
 		measurements.append(-measurement)
-	else:
-		images.append(image)
-		measurements.append(measurement)
+#	else:
+#		images.append(image)
+#		measurements.append(measurement)
 
 def load_data(useAugmented=False, useMultipleCameras=False):
 	images = []
@@ -33,10 +33,12 @@ def load_data(useAugmented=False, useMultipleCameras=False):
 		for line in reader:
 			imagePath = line[0]
 			measurement = float(line[3])
+#			if abs(measurement) <= 0.05 and random.randint(0, 3) != 0:
+#				continue
 			process_image(images, measurements, imagePath, measurement, useAugmented)
 
 			if useMultipleCameras:
-				correction = 0.5
+				correction = 0.1
 
 				leftImagePath = line[1]
 				measurementLeft = measurement + correction
@@ -50,7 +52,7 @@ def load_data(useAugmented=False, useMultipleCameras=False):
 
 
 
-X_train, y_train = load_data(useAugmented=True, useMultipleCameras=True)
+X_train, y_train = load_data(useAugmented=True, useMultipleCameras=False)
 
 from keras.models import Sequential
 from keras.layers import Cropping2D, Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Dropout
@@ -59,26 +61,20 @@ shape = (160, 320, 3)
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=shape))
-model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Cropping2D(cropping=((50, 20), (0, 0))))
 
-model.add(Convolution2D(6, (5, 5), input_shape=shape, activation="relu"))
-model.add(MaxPooling2D())
-
-model.add(Convolution2D(6, (5, 5), activation="relu"))
-model.add(MaxPooling2D())
-
-model.add(Convolution2D(6, (5, 5), activation="relu"))
-model.add(Dropout(0.5))
-
-model.add(Convolution2D(6, (5, 5), activation="relu"))
-model.add(MaxPooling2D())
-
+model.add(Convolution2D(24, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Convolution2D(36, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Convolution2D(48, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Convolution2D(64, (3, 3), strides=(2, 2), activation="relu"))
+model.add(Convolution2D(64, (3, 3), strides=(2, 2), activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
 model.save('model.h5')
